6ccd426548d650272e21c231875504c2d8c3686f
Daniel M <daniemi2@ac.sce.ac.il>
2019-03-25 19:34:19 +0200

    modlim

---
+++ MVC5_proj/Models/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5_proj.Models
+{
+    public class Patient
+    {
+        public string ID { get; set; }
+
+        public string fname { get; set; }
+
+        public string lname { get; set; }
+
+        public string height { get; set; }
+
+        public string weight { get; set; }
+
+        public string email { get; set; }
+
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
